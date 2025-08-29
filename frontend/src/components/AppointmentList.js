6ee10dca73c51d08b81424a7ac57bb8cd3e12c6b
Totalbody <jadewarren801@gmail.com>
2025-08-30 11:20:44 +1200

    Starter AppointmentList component

---
+++ frontend/src/components/AppointmentList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AppointmentList() {
+  return (
+    <div>
+      <h2>Appointment List</h2>
+      {/* List of appointments will go here */}
+    </div>
+  );
+}
+
+export default AppointmentList;
