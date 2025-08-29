ef79932240f509a98b005ead4365c1b74cb7eeb0
Totalbody <jadewarren801@gmail.com>
2025-08-30 11:19:27 +1200

    Starter NoteReview component

---
+++ frontend/src/components/NoteReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoteReview() {
+  return (
+    <div>
+      <h2>Note Review</h2>
+      {/* Note review functionality will go here */}
+    </div>
+  );
+}
+
+export default NoteReview;
