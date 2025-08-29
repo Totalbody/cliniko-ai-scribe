5bd162e593739b4054557151a6733162a973cdff
Totalbody <jadewarren801@gmail.com>
2025-08-30 11:26:58 +1200

    Recorder and NoteReview component stubs

---
+++ src/components/Recorder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Recorder() {
+  return (
+    <div>
+      <h2>Recorder</h2>
+      {/* Audio recording controls will go here */}
+    </div>
+  );
+}
+
+export default Recorder;
