a8e40b61a2bfd5fe3f4ad6b63f8eced420689bcd
Totalbody <jadewarren801@gmail.com>
2025-08-30 11:19:35 +1200

    Starter Recorder component

---
+++ frontend/src/components/Recorder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Recorder() {
+  return (
+    <div>
+      <h2>Recorder</h2>
+      {/* Recording functionality will go here */}
+    </div>
+  );
+}
+
+export default Recorder;
