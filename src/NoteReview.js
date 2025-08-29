3582c7a7d77d1fa9f355ab52fa70169aa00a6c3f
Totalbody <jadewarren801@gmail.com>
2025-08-30 11:27:16 +1200

    Recorder and NoteReview component stubs

---
+++ src/NoteReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoteReview() {
+  return (
+    <div>
+      <h2>Note Review</h2>
+      {/* Generated notes and review controls will go here */}
+    </div>
+  );
+}
+
+export default NoteReview;
